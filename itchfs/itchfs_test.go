3056ce4a89ddf91edba773019c6df2368cf4b2f0
Amos Wenger <amoswenger@gmail.com>
2016-12-03 15:45:32 +0100

    Use new API endpoints, support download_key_id in itchfs urls

--- itchfs/itchfs_test.go
+++ itchfs/itchfs_test.go
@@ -1,6 +1,8 @@
 package itchfs
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -33,7 +35,7 @@ func Test_Resource(t *testing.T) {
 		"/api/1/x/upload/13/download": `{
 			"url": "http://localhost/upload-url"
 		}`,
-		"/api/1/x/download-key/key/download/13": `{
+		"/api/1/x/upload/13/download?download_key_id=key": `{
 			"url": "http://localhost/upload-url-with-key"
 		}`,
 		"/api/1/x/upload/13/download/builds/57": `{
@@ -44,7 +46,7 @@ func Test_Resource(t *testing.T) {
 				"url": "http://localhost/upload-builds-url-patch"
 			}
 		}`,
-		"/api/1/x/download-key/key/download/13/builds/57": `{
+		"/api/1/x/upload/13/download/builds/57?download_key_id=key": `{
 			"signature": {
 				"url": "http://localhost/upload-builds-url-with-key-signature"
 			},
@@ -59,6 +61,10 @@ func Test_Resource(t *testing.T) {
 
 	fakeServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		path := req.URL.Path
+		if len(req.URL.RawQuery) > 0 {
+			path = fmt.Sprintf("%s?%s", path, req.URL.RawQuery)
+		}
+		log.Printf("path = %s, URL = %#v\n", path, req.URL)
 
 		res, ok := responses[path]
 		if !ok {
@@ -124,9 +130,16 @@ func Test_Resource(t *testing.T) {
 
 	testPair("itchfs:///upload/13/download?api_key=x", "http://localhost/upload-url")
 	testPair("itchfs:///download-key/key/download/13?api_key=x", "http://localhost/upload-url-with-key")
+	testPair("itchfs:///upload/13/download?api_key=x&download_key_id=key", "http://localhost/upload-url-with-key")
+
 	testPair("itchfs:///upload/13/download/builds/57/archive?api_key=x", "http://localhost/upload-builds-url-archive")
 	testPair("itchfs:///upload/13/download/builds/57/patch?api_key=x", "http://localhost/upload-builds-url-patch")
+
 	testPair("itchfs:///download-key/key/download/13/builds/57/signature?api_key=x", "http://localhost/upload-builds-url-with-key-signature")
 	testPair("itchfs:///download-key/key/download/13/builds/57/manifest?api_key=x", "http://localhost/upload-builds-url-with-key-manifest")
+
+	testPair("itchfs:///upload/13/download/builds/57/signature?download_key_id=key&api_key=x", "http://localhost/upload-builds-url-with-key-signature")
+	testPair("itchfs:///upload/13/download/builds/57/manifest?api_key=x&download_key_id=key", "http://localhost/upload-builds-url-with-key-manifest")
+
 	testPair("itchfs:///wharf/builds/57/files/90/download?api_key=x", "http://localhost/wharf-build-url")
 }
