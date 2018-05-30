b7b6ef6757a4b35153fd8554706f75d884cd1018
Amos Wenger <amoswenger@gmail.com>
2018-05-30 16:30:35 +0200

    Test struct-based method

--- traits_game.go
+++ traits_game.go
@@ -75,3 +75,48 @@ func GameTraitHookFunc(
 
 	return gt, nil
 }
+
+type GameTraits2 struct {
+	PlatformWindows bool `trait:"p_windows"`
+	PlatformLinux   bool `trait:"p_linux"`
+	PlatformOSX     bool `trait:"p_osx"`
+	PlatformAndroid bool `trait:"p_android"`
+	CanBeBought     bool `trait:"can_be_bought"`
+	HasDemo         bool `trait:"has_demo"`
+	InPressSystem   bool `trait:"in_press_system"`
+}
+
+var _ json.Marshaler = GameTraits2{}
+var _ json.Unmarshaler = (*GameTraits2)(nil)
+
+func (gt GameTraits2) MarshalJSON() ([]byte, error) {
+	var traits []string
+	val := reflect.ValueOf(gt)
+	typ := val.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		if val.Field(i).Bool() {
+			traits = append(traits, typ.Field(i).Tag.Get("trait"))
+		}
+	}
+	return json.Marshal(traits)
+}
+
+func (gt *GameTraits2) UnmarshalJSON(data []byte) error {
+	var traits []string
+	err := json.Unmarshal(data, &traits)
+	if err != nil {
+		return err
+	}
+
+	val := reflect.ValueOf(gt).Elem()
+	typ := val.Type()
+	for _, t := range traits {
+		for i := 0; i < typ.NumField(); i++ {
+			tf := typ.Field(i)
+			if tf.Tag.Get("trait") == t {
+				val.Field(i).SetBool(true)
+			}
+		}
+	}
+	return nil
+}
--- traits_test.go
+++ traits_test.go
@@ -10,6 +10,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_GameTraits2(t *testing.T) {
+	gt1 := GameTraits2{
+		PlatformAndroid: true,
+		HasDemo:         true,
+	}
+
+	marshalled, err := gt1.MarshalJSON()
+	assert.NoError(t, err)
+	assert.EqualValues(t, `["p_android","has_demo"]`, string(marshalled))
+
+	var gt2 GameTraits2
+	err = gt2.UnmarshalJSON(marshalled)
+	assert.NoError(t, err)
+
+	assert.EqualValues(t, gt1, gt2)
+	assert.True(t, gt2.PlatformAndroid)
+	assert.True(t, gt2.HasDemo)
+	assert.False(t, gt2.PlatformWindows)
+	assert.False(t, gt2.CanBeBought)
+}
+
 func Test_GameTraits(t *testing.T) {
 	gt1 := GameTraits{
 		GameTraitPlatformAndroid: true,
@@ -51,4 +72,44 @@ func Test_GameTraits(t *testing.T) {
 	gameRes := &Game{}
 	err = ParseAPIResponse(gameRes, fakeRes)
 	assert.NoError(t, err)
+}
+
+func Benchmark_GameTraits(b *testing.B) {
+	b.Run("map-based", func(b *testing.B) {
+		gt1 := GameTraits{
+			GameTraitPlatformAndroid: true,
+			GameTraitHasDemo:         true,
+		}
+
+		for n := 0; n < b.N; n++ {
+			data, _ := gt1.MarshalJSON()
+			var gt2 GameTraits
+			gt2.UnmarshalJSON(data)
+			if !gt2[GameTraitPlatformAndroid] {
+				panic("missing platform android")
+			}
+			if !gt2[GameTraitHasDemo] {
+				panic("missing has-demo")
+			}
+		}
+	})
+
+	b.Run("struct-based", func(b *testing.B) {
+		gt1 := GameTraits2{
+			PlatformAndroid: true,
+			HasDemo:         true,
+		}
+
+		for n := 0; n < b.N; n++ {
+			data, _ := gt1.MarshalJSON()
+			var gt2 GameTraits2
+			gt2.UnmarshalJSON(data)
+			if !gt2.PlatformAndroid {
+				panic("missing platform android")
+			}
+			if !gt2.HasDemo {
+				panic("missing has-demo")
+			}
+		}
+	})
 }
