5a76bbac4a1af8015644d1255f88f98f617605c3
Amos Wenger <amoswenger@gmail.com>
2018-06-06 11:46:13 +0200

    Additional gamehook test

--- hook_game_test.go
+++ hook_game_test.go
@@ -3,7 +3,9 @@ package itchio
 import (
 	"encoding/json"
 	"testing"
+	"time"
 
+	"github.com/itchio/wharf/wtest"
 	"github.com/mitchellh/mapstructure"
 
 	"github.com/stretchr/testify/assert"
@@ -75,4 +77,49 @@ func Test_GameHook(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.EqualValues(t, ref, unmarshalled)
+
+	// ------------
+}
+
+func Test_GameHookNested(t *testing.T) {
+	type Res struct {
+		Games []*Game `json:"games"`
+	}
+
+	marshalledSane := []byte(`{"games": [{
+			"id": 123,
+			"inPressSystem": true,
+			"platforms": {"linux": "all", "windows": "all"}
+		}]}`)
+
+	intermediate := make(map[string]interface{})
+	err := json.Unmarshal(marshalledSane, &intermediate)
+	wtest.Must(t, err)
+
+	var res Res
+	dec, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		Result: &res,
+		DecodeHook: mapstructure.ComposeDecodeHookFunc(
+			mapstructure.StringToTimeHookFunc(time.RFC3339Nano),
+			GameHookFunc,
+		),
+		WeaklyTypedInput: true,
+	})
+	wtest.Must(t, err)
+
+	err = dec.Decode(intermediate)
+	wtest.Must(t, err)
+
+	assert.EqualValues(t, Res{
+		Games: []*Game{
+			&Game{
+				ID:            123,
+				InPressSystem: true,
+				Platforms: Platforms{
+					Linux:   "all",
+					Windows: "all",
+				},
+			},
+		},
+	}, res)
 }
